09afc0869e409ea066108b579bba3e2e41f2ca2d
Keavy McMinn <keavy@minimetre.com>
2010-02-20 11:11:31 +0000

    updated factory to suit category change

--- test/factories.rb
+++ test/factories.rb
@@ -1,9 +1,5 @@
-Factory.define :category do |f|
-  f.name          "dev"
-end
-
 Factory.define :project do |f|
   f.title          "a new project"
   f.url            "http://www.project.com"
-  f.association    :category
+  f.category       "dev"
 end
